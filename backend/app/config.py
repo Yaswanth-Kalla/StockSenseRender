bbc75dc560e0647595bebfdd8ddcf4cbaedcdd20
Yaswanth-Kalla <155803044+Yaswanth-Kalla@users.noreply.github.com>
2025-06-23 14:59:13 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -12,21 +12,9 @@ TOP_BSE_STOCKS = {
     "ICICIBANK.BSE": "ICICI Bank",
     "SBIN.BSE": "State Bank of India",
     "TATAMOTORS.BSE": "Tata Motors",
-    "SUNPHARMA.BSE": "Sun Pharmaceuticals",
     "BHARTIARTL.BSE": "Bharti Airtel",
     "LT.BSE": "Larsen & Toubro",
-    "BAJFINANCE.BSE": "Bajaj Finance",
     "AXISBANK.BSE": "Axis Bank",
-    "ITC.BSE": "ITC",
-    "HINDUNILVR.BSE": "Hindustan Unilever",
-    "ULTRACEMCO.BSE": "UltraTech Cement",
-    "MARUTI.BSE": "Maruti Suzuki",
-    "TATASTEEL.BSE": "Tata Steel",
-    "JSWSTEEL.BSE": "JSW Steel",
-    "ONGC.BSE": "ONGC",
-    "NTPC.BSE": "NTPC",
-    "POWERGRID.BSE": "Power Grid",
-    "COALINDIA.BSE": "Coal India",
 }
 
 
